369e79474f0246299b0f2585ce3b22d5a9960ffd
shuruiboy <131488879+shuruiboy@users.noreply.github.com>
2023-04-21 16:19:45 -0400

    Update eighth.py

--- eighth.py
+++ eighth.py
@@ -1,38 +1,41 @@
 def parse_assign(tokens):
-    if tokens[0].token_type == 'IDENTIFIER' and tokens[1].lexeme == '=':
-        tokens.pop(0)
-        tokens.pop(0)
-        return parse_expr(tokens)
-    return False
+    if tokens[0].type != 'ID':
+        return False
+    if tokens[1].type != 'ASSIGN':
+        return False
+    if not parse_expr(tokens[2:]):
+        return False
+    return True
 
 def parse_expr(tokens):
-    if parse_term(tokens):
-        while tokens[0].lexeme in ['+', '-']:
-            tokens.pop(0)
-            if not parse_term(tokens):
-                return False
-        return True
-    return False
+    if not parse_term(tokens):
+        return False
+    while tokens[0].type in {'PLUS', 'MINUS'}:
+        tokens = tokens[1:]
+        if not parse_term(tokens):
+            return False
+    return True
 
 def parse_term(tokens):
-    if parse_fact(tokens):
-        while tokens[0].lexeme in ['*', '/', '%']:
-            tokens.pop(0)
-            if not parse_fact(tokens):
-                return False
-        return True
-    return False
+    if not parse_fact(tokens):
+        return False
+    while tokens[0].type in {'MULT', 'DIV', 'MOD'}:
+        tokens = tokens[1:]
+        if not parse_fact(tokens):
+            return False
+    return True
 
 def parse_fact(tokens):
-    if tokens[0].token_type in ['IDENTIFIER', 'INTEGER', 'FLOAT']:
-        tokens.pop(0)
+    if tokens[0].type == 'ID':
         return True
-    elif tokens[0].lexeme == '(':
-        tokens.pop(0)
-        if not parse_expr(tokens):
+    if tokens[0].type == 'INT_LIT':
+        return True
+    if tokens[0].type == 'FLOAT_LIT':
+        return True
+    if tokens[0].type == 'LPAREN':
+        if not parse_expr(tokens[1:]):
             return False
-        if tokens[0].lexeme != ')':
+        if tokens[1].type != 'RPAREN':
             return False
-        tokens.pop(0)
         return True
     return False
