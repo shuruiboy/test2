fa7e28e74a1e4f469ca51c8612b1dee7c206672b
shuruiboy <131488879+shuruiboy@users.noreply.github.com>
2023-04-21 16:14:05 -0400

    Update forth.py

--- forth.py
+++ forth.py
@@ -1,21 +1,12 @@
-def parse_while_loop(tokens):
-    # Make sure the token list starts with "while"
-    if tokens[0] != "while":
+def while_loop(tokens):
+    if len(tokens) < 5 or tokens[0].value != 'while' or tokens[1].value != '(':
         return False
     
-    # Find the index of the closing parenthesis
-    closing_paren_index = find_closing_paren(tokens, 1)
-    if closing_paren_index == -1:
-        return False
+    i = 2
+    while i < len(tokens):
+        if tokens[i].value == ')':
+            block_result = block(tokens[i+1:])
+            return block_result
+        i += 1
     
-    # Parse the boolean expression inside the parentheses
-    bool_expr_tokens = tokens[2:closing_paren_index]
-    if not parse_bool_expr(bool_expr_tokens):
-        return False
-    
-    # Parse the block inside the while loop
-    block_tokens = tokens[closing_paren_index+1:]
-    if not parse_block(block_tokens):
-        return False
-    
-    return True
+    return False
