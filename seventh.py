857ae7d5037547a2f521fae42fd6073bb1e64b08
shuruiboy <131488879+shuruiboy@users.noreply.github.com>
2023-04-21 16:18:59 -0400

    Update seventh.py

--- seventh.py
+++ seventh.py
@@ -1,28 +1,29 @@
-class Token:
-    def __init__(self, token_type, lexeme):
-        self.token_type = token_type
-        self.lexeme = lexeme
-
-    def __repr__(self):
-        return f"{self.token_type}({self.lexeme})"
-
-
-def parse(tokens):
-    def match(token_type):
-        nonlocal index
-        if index >= len(tokens):
-            return False
-        if tokens[index].token_type == token_type:
-            index += 1
+def parse_declare(tokens):
+    def match(expected_type):
+        nonlocal current_token
+        if current_token and current_token.token_type == expected_type:
+            current_token = next_token()
             return True
         return False
-
-    def DECLARE():
-        if match('DATA_TYPE') and match('IDENTIFIER'):
-            while match('COMMA') and match('IDENTIFIER'):
-                pass
+    
+    def next_token():
+        nonlocal current_token_index
+        current_token_index += 1
+        return tokens[current_token_index] if current_token_index < len(tokens) else None
+    
+    def ID_list():
+        if match('IDENTIFIER'):
+            while match('COMMA'):
+                if not match('IDENTIFIER'):
+                    return False
             return True
         return False
+    
+    current_token_index = 0
+    current_token = tokens[current_token_index]
+    
+    if match('DATATYPE') and match('IDENTIFIER') and ID_list():
+        return True
+    
+    return False
 
-    index = 0
-    return DECLARE()
